27c845c87ec05c04c7c00a614b9c07c39ae90686
ThuongHoai081 <th316942@gmail.com>
2025-09-12 01:03:54 +0700

    feat(user): add DTOs for user

---
+++ src/modules/user/dto/user.dto.ts
@@ -0,0 +1,46 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { User } from '../domain/user.domain';
+
+export class UserDto {
+  @ApiProperty({ example: 1, description: 'Unique identifier of the user' })
+  readonly id: number;
+
+  @ApiProperty({ example: 'John', description: 'First name of the user' })
+  readonly firstName: string;
+
+  @ApiProperty({ example: 'Doe', description: 'Last name of the user' })
+  readonly lastName: string;
+
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'Email of the user',
+  })
+  readonly email: string;
+
+  @ApiPropertyOptional({
+    example: '2025-09-10T12:00:00Z',
+    description: 'Date when the user was created',
+  })
+  readonly createdAt?: Date;
+
+  @ApiPropertyOptional({
+    example: '2025-09-10T12:00:00Z',
+    description: 'Date when the user was last updated',
+  })
+  readonly updatedAt?: Date;
+
+  static fromDomain(user: User): UserDto {
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+
+  static fromDomains(users: User[]): UserDto[] {
+    return users.map((user) => this.fromDomain(user));
+  }
+}
