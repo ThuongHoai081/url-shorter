a2591e6bd037f0b4c54f5ecd68430c1c95c52c99
ThuongHoai081 <th316942@gmail.com>
2025-09-12 01:10:17 +0700

    feat(user): add query handlers index and e2e test for user

---
+++ src/modules/user/queries/handlers/index.ts
@@ -0,0 +1,4 @@
+import { UserGetHandler } from './user-get.handler';
+import { UsersGetAllHandler } from './users-get-all.handler';
+
+export const QueryHandlers = [UserGetHandler, UsersGetAllHandler];
