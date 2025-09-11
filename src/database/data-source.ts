2b48bb5b20e4703d6f7df6457084996f8d65dc64
ThuongHoai081 <th316942@gmail.com>
2025-09-12 00:59:11 +0700

    chore: update config

---
+++ src/database/data-source.ts
@@ -0,0 +1,21 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config as dotenvConfig } from 'dotenv';
+import { SeederOptions } from 'typeorm-extension';
+
+dotenvConfig({ path: '.env' });
+
+const options = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.DATABASE_PORT ?? '5433'),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  seeds: [__dirname + '/seeds/**/*{.ts,.js}'],
+  factories: [__dirname + '/factories/**/*{.ts,.js}'],
+  synchronize: false,
+} satisfies DataSourceOptions & SeederOptions;
+
+export const AppDataSource = new DataSource(options);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix(process.env.API_PREFIX ?? 'api/v1');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription('API documentation for URL Shortener project')
+    .setVersion('1.0')
+    .addTag('api/v1')
+    .build();
+
+  function documentFactory() {
+    return SwaggerModule.createDocument(app, config);
+  }
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
