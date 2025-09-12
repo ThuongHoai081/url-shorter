13fc256418bea1b21b27c2722aef01a0a378af3c
ThuongHoai081 <th316942@gmail.com>
2025-09-12 16:51:30 +0700

    feat(domain): add service domain

--- src/modules/domain/domain.module.ts
+++ src/modules/domain/domain.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DomainService } from './domain.service';
 import { DomainController } from './domain.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DomainEntity } from './entities/domain.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DomainEntity])],
   controllers: [DomainController],
   providers: [DomainService],
+  exports: [DomainService],
 })
 export class DomainModule {}
--- src/modules/domain/domain.service.ts
+++ src/modules/domain/domain.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DomainEntity } from './entities/domain.entity';
 
 @Injectable()
-export class DomainService {}
+export class DomainService {
+  constructor(
+    @InjectRepository(DomainEntity)
+    private readonly domainRepository: Repository<DomainEntity>,
+  ) {}
+
+  async createOrGetDomainId(originalUrl: string): Promise<number> {
+    const domainName = this.extractDomain(originalUrl);
+
+    let domainEntity = await this.domainRepository.findOneBy({
+      name: domainName,
+    });
+
+    if (!domainEntity) {
+      domainEntity = this.domainRepository.create({ name: domainName });
+      domainEntity = await this.domainRepository.save(domainEntity);
+    }
+
+    const domainId = domainEntity.id;
+
+    return domainId;
+  }
+
+  private extractDomain(originalUrl: string): string {
+    const url = new URL(originalUrl);
+
+    return url.hostname;
+  }
+}
