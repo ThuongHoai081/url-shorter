0762c3f7e2647407de2d3826db6f5fedd9b97d73
ThuongHoai081 <th316942@gmail.com>
2025-09-12 08:57:00 +0700

    fix(user): remove lib

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,16 +1,12 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-import { APP_PIPE } from '@nestjs/core';
-import { CqrsModule } from '@nestjs/cqrs';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), CqrsModule],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [
-    UserService,
-  ],
+  providers: [UserService],
 })
 export class UserModule {}
